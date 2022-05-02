4ba89cec8a523a937f17781b95e6327c90ba04ae
aniruddha2008@gmail.com <ertleast@001>
2022-05-02 13:00:13 +0530

    generated all qr codes

--- index.php
+++ index.php
@@ -5,16 +5,34 @@ require('dbconnect.php');
 require('phpqrcode/qrlib.php');
 
 
-//Prepare QR data
-$qr_data  = "HelloWorld123";
-//Display in Browser
-#QRcode::png($qr_data);
-//Save to server path
-QRcode::png($qr_data, 'test.png', 'L', 4, 2);
-//Get server path
-$directory = dir(getcwd());
-//Display Server path where saved
-echo "Generated QR code image in location: ".$directory->path;
+
+$sql = "SELECT `item id`,`PAGE NO/SLNO` FROM `isr`";
+$result = $conn->query($sql);
+echo $result->num_rows;
+$count = 0;
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        #echo "ITEM ID: " . $row["item id"]." "."PAGE NO/SLNO: " .$row["PAGE NO/SLNO"]."<br>";
+
+        //Prepare QR data
+        $qr_data  = $row["item id"]."*****".$row["PAGE NO/SLNO"];
+        $count += 1;
+        $filename = "qr_".$count;
+        //Display in Browser
+        #QRcode::png($qr_data);
+        //Save to server path
+        QRcode::png($qr_data, $filename, 'L', 4, 2);
+        //Get server path
+        $directory = dir(getcwd());
+        //Display Server path where saved
+        echo "Generated QR code image in location: ".$directory->path;
+    }
+} else {
+    echo "0 results";
+}
+
+
 
 
 
