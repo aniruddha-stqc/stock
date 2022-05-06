8b98738db9c3a47b5a534ff37c4f78d8b8a974fa
aniruddha2008@gmail.com <ertleast@001>
2022-05-06 13:15:29 +0530

    added pdf library

---
+++ print.php
@@ -0,0 +1,66 @@
+<?php
+
+require('fpdf184/fpdf.php');
+
+$pdf = new FPDF();
+
+$pdf->AddPage();
+
+$pdf->SetFont('Arial', '', 12);
+
+
+$pdf->Cell(55, 5, 'Reference Code', 0, 0);
+$pdf->Cell(58, 5, ': 026ETY', 0, 0);
+$pdf->Cell(25, 5, 'Date', 0, 0);
+$pdf->Cell(52, 5, ': 2018-12-24 11:47:10 AM', 0, 1);
+
+$pdf->Cell(55, 5, 'Amount', 0, 0);
+$pdf->Cell(58, 5, ': 2674', 0, 0);
+$pdf->Cell(25, 5, 'Channel', 0, 0);
+$pdf->Cell(52, 5, ': WEB', 0, 1);
+
+$pdf->Cell(55, 5, 'Status', 0, 0);
+$pdf->Cell(58, 5, ': Complete', 0, 1);
+
+$pdf->Line(10, 30, 200, 30);
+
+$pdf->Ln(10);
+$pdf->Cell(55, 5, 'Product Id', 0, 0);
+$pdf->Cell(58, 5, ': 64351-84503', 0, 1);
+
+$pdf->Cell(55, 5, 'Tax Amount', 0, 0);
+$pdf->Cell(58, 5, ': 0', 0, 1);
+
+$pdf->Cell(55, 5, 'Product Service Charge', 0, 0);
+$pdf->Cell(58, 5, ': 0', 0, 1);
+
+$pdf->Cell(55, 5, 'Product Delivery Charge', 0, 0);
+$pdf->Cell(58, 5, ': 0', 0, 1);
+
+$pdf->Line(10, 60, 200, 60);
+
+$pdf->Ln(10);//Line break
+$pdf->Cell(55, 5, 'Paid by', 0, 0);
+$pdf->Cell(58, 5, ': Nawaraj Shah', 0, 1);
+
+$pdf->Line(155, 75, 195, 75);
+$pdf->Ln(5);//Line break
+$pdf->Cell(140, 5, '', 0, 0);
+$pdf->Cell(50, 5, ': Signature', 0, 1, 'C');
+
+$image1 = "QR/qr_001.png";
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Ln(40);//Line break
+
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
+
+$pdf->Output();
+?>
